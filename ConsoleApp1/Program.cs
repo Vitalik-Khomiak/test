613b5775c4288ad86d57cfee1d81cc08f69f4842
Vitalik-Khomiak <vitaliikhomiak@gmail.com>
2020-04-20 19:50:10 +0300

    lab

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -90;
+            Console.WriteLine($"| x  \t||    f(x) \t\t\t|");
+            do
+            {
+
+                double y = Math.Pow(x, 2) * Math.Cos(x);
+
+
+                Console.WriteLine($"|{x}  \t||    {y} \t|");
+                x += -18;
+
+            } while ( x >= -270 && x <= -90 );
+
+        }
+    }
+}
