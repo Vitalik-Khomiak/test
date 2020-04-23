73027bb5ac8e38212bb13e79a1734e4383a3a7be
Vitalik-Khomiak <vitaliikhomiak@gmail.com>
2020-04-23 19:47:39 +0300

    LAB

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,28 +2,96 @@
 
 namespace ConsoleApp1
 {
-    public class Program
+   public class Program
     {
-        static public int Example(int a, int b)
+
+        
+
+        
+        public static void test(int[,]c)
         {
-            int c = a + b;
-            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " +
-            c.ToString());
-            return c;
+            int max = c[1, 1];
+            int max_i = 0;
+
+            int suma = 0;
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (max < c[i, j])
+                    {
+                        max_i = i;
+                        max = c[i, j];
+
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Max = " + max);
+            Console.WriteLine("Max_i = " + max_i);
+            for (int i = 0; i < 3; i++)
+            {
+
+                suma += c[i, i];
+
+            }
+            Console.WriteLine("Suma = " + suma);
+
+
+            for (int i = 0; i < 3; i++) ;
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    c[max_i, j] = suma;
+                }
+
+            }
+
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+
+                    Console.Write(c[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
         }
 
 
-        static void Main(string[] args)
+
+        public static void Main()
         {
 
-            Console.WriteLine("Введiть перше число");
-            string S1 = Console.ReadLine();
-            int a = int.Parse(S1);
-            Console.WriteLine("Введiть друге число");
-            string S2 = Console.ReadLine();
-            int b = int.Parse(S2);
-            Example(a, b);
-            Console.ReadLine();
+
+
+            int N = 3;
+            int[,] c = new int[N, N];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    Console.Write($"Data[{j}]=");
+                    c[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+            test(c);
         }
     }
 }
+
+        
+            
+
+
+
