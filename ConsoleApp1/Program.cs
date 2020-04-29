946808d0780ee7bfb0d1d9ddee045c92aa862701
Vitalik-Khomiak <Vitaliikhomiakk@gmail.com>
2020-04-29 19:56:19 +0300

    19.56

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,96 +2,105 @@
 
 namespace ConsoleApp1
 {
-   public class Program
+    public class Program
     {
-
-        
-
-        
-        public static void test(int[,]c)
-        {
-            int max = c[1, 1];
-            int max_i = 0;
-
-            int suma = 0;
-            for (int i = 0; i < 3; i++)
-            {
-
-                for (int j = 0; j < 3; j++)
-                {
-                    if (max < c[i, j])
-                    {
-                        max_i = i;
-                        max = c[i, j];
-
-                    }
-
-                }
-
-            }
-
-            Console.WriteLine("Max = " + max);
-            Console.WriteLine("Max_i = " + max_i);
-            for (int i = 0; i < 3; i++)
-            {
-
-                suma += c[i, i];
-
-            }
-            Console.WriteLine("Suma = " + suma);
-
-
-            for (int i = 0; i < 3; i++) ;
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    c[max_i, j] = suma;
-                }
-
-            }
-
-
-            for (int i = 0; i < 3; i++)
-            {
-
-                for (int j = 0; j < 3; j++)
-                {
-
-                    Console.Write(c[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-
-
-
-        }
-
 
 
         public static void Main()
         {
 
-
-
-            int N = 3;
-            int[,] c = new int[N, N];
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine("ВВедiть розмiр масиву");
+            Console.Write("Рядки - ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Стовпцi - ");
+            m = Convert.ToInt32(Console.ReadLine());
+            if (n == m)
             {
-                for (int j = 0; j < 3; j++)
-                {
 
-                    Console.Write($"Data[{j}]=");
-                    c[i, j] = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                int[,] c = new int[n, m];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write($"Data[{i},{j}]=");
+                        c[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
                 }
 
+
+
+                int max = c[0, 0];
+                int max_i = 0;
+
+                int suma = 0;
+                for (int i = 0; i < n; i++)
+                {
+
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (max < c[i, j])
+                        {
+                            max_i = i;
+                            max = c[i, j];
+
+                        }
+
+                    }
+
+                }
+
+                Console.WriteLine("Max = " + max);
+                Console.WriteLine("Max_i = " + max_i);
+                for (int i = 0; i < n; i++)
+                {
+
+                    suma += c[i, i];
+
+                }
+                Console.WriteLine("Suma = " + suma);
+
+
+                for (int i = 0; i < 3; i++) ;
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        c[max_i, j] = suma;
+                    }
+
+                }
+
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    for (int j = 0; j < m; j++)
+                    {
+
+                        Console.Write(c[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+
+
             }
-            test(c);
+            else
+            {
+                Console.WriteLine("Некоректний розмiр масиву");
+            }
+
+
         }
     }
 }
 
-        
-            
+
+
 
 
 
