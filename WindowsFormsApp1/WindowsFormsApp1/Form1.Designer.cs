9f62cd4f7ce3d5b9e8930815804b9667a0caa5dd
Vitalik-Khomiak <vitaliikhomiakk@gmail.com>
2020-05-31 20:00:38 +0300

    lab#8.2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SortName = new System.Windows.Forms.Button();
+            this.SortPrice = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.Sortkitchen = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add new dish";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(143, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 60);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Delete dish";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Show menu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(78, 415);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 32);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Main menu";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // SortName
+            // 
+            this.SortName.Location = new System.Drawing.Point(3, 134);
+            this.SortName.Name = "SortName";
+            this.SortName.Size = new System.Drawing.Size(134, 38);
+            this.SortName.TabIndex = 5;
+            this.SortName.Text = "Sort by name ";
+            this.SortName.UseVisualStyleBackColor = true;
+            this.SortName.Click += new System.EventHandler(this.SortName_Click);
+            // 
+            // SortPrice
+            // 
+            this.SortPrice.Location = new System.Drawing.Point(143, 134);
+            this.SortPrice.Name = "SortPrice";
+            this.SortPrice.Size = new System.Drawing.Size(146, 38);
+            this.SortPrice.TabIndex = 6;
+            this.SortPrice.Text = "Sort by gram";
+            this.SortPrice.UseVisualStyleBackColor = true;
+            this.SortPrice.Click += new System.EventHandler(this.SortPrice_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(311, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 435);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 178);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(138, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(138, 229);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(78, 325);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(146, 43);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Confirm";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Sortkitchen
+            // 
+            this.Sortkitchen.Location = new System.Drawing.Point(110, 214);
+            this.Sortkitchen.Name = "Sortkitchen";
+            this.Sortkitchen.Size = new System.Drawing.Size(146, 37);
+            this.Sortkitchen.TabIndex = 12;
+            this.Sortkitchen.Text = "Sort by kitchen";
+            this.Sortkitchen.UseVisualStyleBackColor = true;
+            this.Sortkitchen.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(138, 257);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(151, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(138, 285);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(151, 22);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "cooking_time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "gram";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "kitchen";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(78, 325);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(146, 43);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Confirm";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(78, 325);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 43);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "Confirm";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Sortkitchen);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SortPrice);
+            this.Controls.Add(this.SortName);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button SortName;
+        private System.Windows.Forms.Button SortPrice;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button Sortkitchen;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public class Menu_Data
+        {
+            public string name { get; set; }
+            public string price { get; set; }
+            public string cooking_time { get; set; }
+            public string gram { get; set; }
+            public string kitchen { get; set; }
+        }
+
+        private static string FileName = "Data.json";
+        private static string FilePath = @"Data.json";
+        public Form1()
+        {
+            InitializeComponent();
+            EditLabel(0);
+        }
+
+
+        void add_menu(string Name, string Price, string Cooking_time, string Gram, string Kitchen)
+        {
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+
+            data.Add(new Menu_Data { name = Name, price = Price, cooking_time = Cooking_time, gram = Gram, kitchen = Kitchen });
+            string serialize = JsonConvert.SerializeObject(data, Formatting.Indented);
+            if (serialize.Count() > 1)
+            {
+                if (!File.Exists(FileName))
+                {
+                    File.Create(FileName).Close();
+                }
+                File.WriteAllText(FilePath, serialize, Encoding.UTF8);
+            }
+            EditLabel(0);
+            MainButton(1);
+
+        }
+
+        void delete_menu(string Name)
+        {
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+            data.RemoveAll(x => x.name == Name);
+            string serialize = JsonConvert.SerializeObject(data, Formatting.Indented);
+            if (serialize.Count() > 1)
+            {
+                if (!File.Exists(FileName))
+                {
+                    File.Create(FileName).Close();
+                }
+                File.WriteAllText(FilePath, serialize, Encoding.UTF8);
+            }
+            EditLabel(0);
+            MainButton(1);
+        }
+
+        void search_menu(string Name)
+        {
+            textBox1.Text = "";
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+            Menu_Data FoundData = data.Find(found => found.name == Name);
+            string info = "     " + FoundData.name + "\t\t" + FoundData.price + "     \t   " + FoundData.cooking_time + "\t\t" + FoundData.gram + "                 " + FoundData.kitchen;
+
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("      Name     │   price     │ cooking_time │    gram    │ kitchen");
+            textBox1.AppendText(Environment.NewLine);
+
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText(info);
+            textBox1.AppendText(Environment.NewLine);
+
+
+            string serialize = JsonConvert.SerializeObject(data, Formatting.Indented);
+            if (serialize.Count() > 1)
+            {
+                if (!File.Exists(FileName))
+                {
+                    File.Create(FileName).Close();
+                }
+                File.WriteAllText(FilePath, serialize, Encoding.UTF8);
+            }
+            EditLabel(0);
+            MainButton(1);
+        }
+
+        void MainButton(int check)
+        {
+            if (check == 0)
+            {
+                button1.Enabled = false;
+                button4.Enabled = false;
+                button3.Enabled = false;
+                button2.Enabled = false;
+            }
+            if (check == 1)
+            {
+                button1.Enabled = true;
+                button4.Enabled = true;
+                button3.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+
+        void EditLabel(int check)
+        {
+            if (check == 0)
+            {
+                label1.Hide();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                textBox2.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                SortName.Hide();
+                button8.Hide();
+                button7.Hide();
+                button6.Hide();
+
+                SortName.Hide();
+                SortPrice.Hide();
+                Sortkitchen.Hide();
+            }
+            if (check == 1)
+            {
+                label1.Show();
+                label2.Show();
+                label3.Show();
+                label4.Show();
+                label5.Show();
+                textBox2.Show();
+                textBox3.Show();
+                textBox4.Show();
+                textBox5.Show();
+                textBox6.Show();
+                SortName.Show();
+                
+                SortName.Hide();
+                SortPrice.Hide();
+                Sortkitchen.Hide();
+            }
+            if (check == 2)
+            {
+               
+                label1.Show();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                textBox2.Show();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                SortName.Hide();
+
+                SortName.Hide();
+                SortPrice.Hide();
+                Sortkitchen.Hide();
+            }
+            if (check == 3)
+            {
+
+                label1.Show();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                textBox2.Show();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                SortName.Hide();
+               
+                SortName.Hide();
+                SortPrice.Hide();
+                Sortkitchen.Hide();
+            }
+            if (check == 4)
+            {
+
+                label1.Hide();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                textBox2.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                SortName.Hide();
+                button8.Hide();
+                SortName.Show();
+                SortPrice.Show();
+                Sortkitchen.Show();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EditLabel(1);
+            MainButton(0);
+            button8.Show();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EditLabel(4);
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+            textBox1.Text = "";
+       
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("     Name      │       price     │ cooking_time │\tgram    │\tkitchen");
+            textBox1.AppendText(Environment.NewLine);
+    
+            for (int i = 0; i < data.Count; i++)
+            {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText("     " + data[i].name + "\t\t" + data[i].price + "     \t   " + data[i].cooking_time + "\t\t" + data[i].gram + "             " + data[i].kitchen);
+                textBox1.AppendText(Environment.NewLine);
+              
+            }
+            textBox1.AppendText(Environment.NewLine);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+            List<Menu_Data> SortData = data.OrderBy(o => o.kitchen).ToList();
+            textBox1.Text = "";
+
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("     Name      │       price     │ cooking_time │\tgram    │ \tkitchen");
+            textBox1.AppendText(Environment.NewLine);
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText("     " + SortData[i].name + "\t\t" + SortData[i].price + "     \t   " + SortData[i].cooking_time + "\t\t" + SortData[i].gram + "                 " + SortData[i].kitchen);
+                textBox1.AppendText(Environment.NewLine);
+            }
+            textBox1.AppendText(Environment.NewLine);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MainButton(0);
+            EditLabel(2);
+            button6.Show();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+            MainButton(0);
+            EditLabel(2);
+            button7.Show();
+        }
+
+        private void SortName_Click(object sender, EventArgs e)
+        {
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+            List<Menu_Data> SortData = data.OrderBy(o => o.name).ToList();
+            textBox1.Text = "";
+            
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("     Name      │       price     │ cooking_time │\tgram    │ \tkitchen");
+            textBox1.AppendText(Environment.NewLine);
+           
+            for (int i = 0; i < data.Count; i++)
+            {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText("     " + SortData[i].name + "\t\t" + SortData[i].price + "     \t   " + SortData[i].cooking_time + "\t\t" + SortData[i].gram + "                 " + SortData[i].kitchen);
+                textBox1.AppendText(Environment.NewLine);
+            }
+            textBox1.AppendText(Environment.NewLine);
+           
+        }
+
+        private void SortPrice_Click(object sender, EventArgs e)
+        {
+            var data = JsonConvert.DeserializeObject<List<Menu_Data>>(File.ReadAllText(FilePath));
+            List<Menu_Data> SortData = data.OrderBy(o => o.gram).ToList();
+            textBox1.Text = "";
+
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("     Name      │       price     │ cooking_time │\tgram    │ \tkitchen");
+            textBox1.AppendText(Environment.NewLine);
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText("     " + SortData[i].name + "\t\t" + SortData[i].price + "     \t   " + SortData[i].cooking_time + "\t\t" + SortData[i].gram + "                 " + SortData[i].kitchen);
+                textBox1.AppendText(Environment.NewLine);
+            }
+            textBox1.AppendText(Environment.NewLine);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            EditLabel(0);
+            MainButton(1);
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            add_menu(textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            delete_menu(textBox2.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           search_menu(textBox2.Text);
+        }
+    }
+}
+
